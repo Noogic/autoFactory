892aad95beeb19d6f6e1d585960c33eabc061f8e
Noogic <noogic@gmail.com>
2015-11-26 00:08:28 +0100

    Update simple values test

--- test/FactoryTest.php
+++ test/FactoryTest.php
@@ -23,15 +23,20 @@ class FactoryTest extends \PHPUnit_Framework_TestCase {
         $this->assertInstanceOf($class, $instance);
     }
 
-    public function test_factory_get_simple_instance_with_integer(){
+    public function test_factory_get_simple_instance_with_values(){
         $class = $this->classes_namespace . 'Test_SimpleClassParams';
         $data = [
-          'a' => 5
+            'a' => 5,
+            'b' => 'string',
+            'c' => [1, 'two', [3, 4]]
         ];
 
         $instance = $this->factory->get($class, $data);
 
 
         $this->assertEquals($data['a'], $instance->a);
+        $this->assertEquals($data['b'], $instance->b);
+        $this->assertEquals($data['c'], $instance->c);
+        $this->assertNull($instance->d);
     }
 }
